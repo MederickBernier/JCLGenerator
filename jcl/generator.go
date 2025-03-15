570f4cc1ca4440e9cf60ed774f79cf22d99de663
Mederick Bernier <mederick.bernier@hotmail.ca>
2025-03-15 07:21:24 -0400

    implemented the generator

--- jcl/generator.go
+++ jcl/generator.go
@@ -1,1 +1,25 @@
 package jcl
+
+import (
+	"os"
+	"text/template"
+)
+
+func GenerateJCL(outputFilename string, params JCLParameters) error {
+	tmpl, err := template.ParseFiles("templates/template.txt")
+	if err != nil {
+		return err
+	}
+
+	if len(outputFilename) < 4 || outputFilename[len(outputFilename)-4:] != ".jcl" {
+		outputFilename += ".jcl"
+	}
+
+	file, err := os.Create(outputFilename)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	return tmpl.Execute(file, params)
+}
